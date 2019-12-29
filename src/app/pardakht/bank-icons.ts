a5e0f1ceb5ec2f9850ca81c9bb98119c609c4e64
JCln <ghmantera@gmail.com>
2019-12-29 22:08:25 +0330

    view b lotus not working yet

--- src/app/pardakht/bank-icons.ts
+++ src/app/pardakht/bank-icons.ts
@@ -2,7 +2,7 @@ import { IbankIcons } from "./ibank-icons";
 
 export const bankIcons: IbankIcons[] = [
     {
-        imgUrl: '../../../../assets/bankIcons/parsian.png', name: 'بانک پارسیان', linkToSite: 'http://bmi.ir'
+        imgUrl: '../../../../assets/bankIcons/parsian.jpg', name: 'پارسیان', linkToSite: 'http://bmi.ir'
     },
     {
         imgUrl: '../../../../assets/bankIcons/bpm.png', name: 'به پرداخت ملت', linkToSite: '#'
