d748b46e3a1c3d4c1fad68471add6d98a90260a1
JCln <ghmantera@gmail.com>
2020-02-17 19:54:07 +0330

    Installment view almost complete

--- src/app/pardakht/installment/installment.component.ts
+++ src/app/pardakht/installment/installment.component.ts
@@ -7,14 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InstallmentComponent implements OnInit {
   spinnerBoolean = false;
+  showMoreButton = false;
+
   testObject: any = [
     { amount: '2990000', billId: '10018315', Date: '98/10/10', deadLine: '98/11/12', isPayed: false },
-    { amount: '983000', billId: '10018315', Date: '98/10/10', deadLine: '98/11/12', isPayed: false },
     { amount: '983000', billId: '10018315', Date: '98/10/10', deadLine: '98/11/12', isPayed: true },
-    { amount: '983000', billId: '10018315', Date: '98/10/10', deadLine: '98/11/12', isPayed: false }
+    { amount: '983000', billId: '10018315', Date: '98/10/10', deadLine: '98/11/12', isPayed: true },
+    { amount: '983000', billId: '10018315', Date: '98/10/10', deadLine: '98/11/12', isPayed: true }
   ]
   constructor() { }
 
+  showMoreButtonClicked = (): void => {
+    this.showMoreButton = !this.showMoreButton;
+    scroll(0, 1000);
+  }
+  
   ngOnInit() {
   }
 
