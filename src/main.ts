fe34e4b3f9f8e6cf4fe6c284c95508e60ca4c4f2
JCln <ghmantera@gmail.com>
2020-04-16 15:04:35 +0430

    no log on prod + updated to angular9.1.1

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
-  window.console.log = function () { };
+  window.console.log = window.console.warn = window.console.info = window.console.error = function () { };
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
