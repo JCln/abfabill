e4cee58ded6612793fa8b54c5f248aa6d6d16716
JCln <ghmantera@gmail.com>
2020-04-19 12:09:41 +0430

    back to previous page and not previous link

--- src/app/_layout/header/header.component.ts
+++ src/app/_layout/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  previousPage: string;
+
   changeBackImg = () => {
     if (screen.width > 549) {
       return;
@@ -22,6 +24,10 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.goToHome();
+  }
+  goToHome = () => {
+    this.previousPage = window.location.pathname.split('/')[1];
   }
 
 }
