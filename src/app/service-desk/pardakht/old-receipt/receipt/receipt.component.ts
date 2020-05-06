1965871bbb2be147ed5c26ad7db0c1f709cb3590
JCln <ghmantera@gmail.com>
2020-05-06 23:27:06 +0430

    receipt changed for dummy fixed

--- src/app/service-desk/pardakht/old-receipt/receipt/receipt.component.ts
+++ src/app/service-desk/pardakht/old-receipt/receipt/receipt.component.ts
@@ -18,6 +18,24 @@ export class ReceiptComponent implements OnInit {
       this.childEl = res;
     });
   }
+  fixedWidth = () => {
+    let main = document.querySelector('.main') as HTMLElement;
+    let _numbers1 = document.querySelector('._first') as HTMLElement;
+    let _numbers2 = document.querySelector('._second') as HTMLElement;
+    let _numbers3 = document.querySelector('._third') as HTMLElement;
+    let _numbers4 = document.querySelector('._forth') as HTMLElement;
+
+    main.style.display = "grid";
+    main.style.gridTemplateColumns = "repeat(4, minmax(20rem , 1fr))";
+    main.style.overflow = "auto";
+    main.style.justifyContent = "inherit";
+
+    _numbers1.style.width = "100%";
+    _numbers2.style.width = "100%";
+    _numbers3.style.width = "100%";
+    _numbers4.style.width = "100%";
+  }
+
   ngOnInit() {
   }
   exportAsPrint(): void {
