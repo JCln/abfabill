75c79c2e8cc156e152648582d0890762608e304f
JCln <ghmantera@gmail.com>
2020-05-10 02:33:03 +0430

    un use componet

--- src/app/service-desk/service-desk.module.ts
+++ src/app/service-desk/service-desk.module.ts
@@ -3,10 +3,9 @@ import { SharedModule } from 'src/app/shared/shared.module';
 
 import { ServiceDeskRoutingModule } from './service-desk-routing.module';
 import { ServicesComponent } from './services/services.component';
-import { MemberInfoPipe } from './pipes/member-info.pipe';
 
 @NgModule({
-  declarations: [ServicesComponent, MemberInfoPipe],
+  declarations: [ServicesComponent],
   imports: [
     SharedModule,
     ServiceDeskRoutingModule
