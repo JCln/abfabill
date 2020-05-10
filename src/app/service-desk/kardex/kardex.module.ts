f2f16a5ca4a2525672cb957180eb4be2c4af9af2
JCln <ghmantera@gmail.com>
2020-05-11 00:55:37 +0430

    chartjs added(bar & bubble), need to get dynamic data

--- src/app/service-desk/kardex/kardex.module.ts
+++ src/app/service-desk/kardex/kardex.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
 
 import { CurrencySplitterModule } from './../../shared/currency-splitter.module';
 import { SharedModule } from './../../shared/shared.module';
 import { KardexRoutingModule } from './kardex-routing.module';
 import { KardexComponent } from './kardex.component';
+import { BubbleComponent } from './chart/bubble/bubble.component';
+import { BarComponent } from './chart/bar/bar.component';
 
 
 @NgModule({
-  declarations: [KardexComponent],
+  declarations: [KardexComponent, BubbleComponent, BarComponent],
   imports: [
     SharedModule,
     CurrencySplitterModule,
+    ChartsModule,
     KardexRoutingModule
   ]
 })
