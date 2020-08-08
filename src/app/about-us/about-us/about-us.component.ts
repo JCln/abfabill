91f2e1a38e738449f41d313ede15ef52b2aa10bf
JCln <ghmantera@gmail.com>
2020-08-08 12:46:29 +0430

    edited about us => clear DI

--- src/app/about-us/about-us/about-us.component.ts
+++ src/app/about-us/about-us/about-us.component.ts
@@ -1,21 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
-import { IAboutUs } from './../../interfaces/Iabout-us';
-import { AboutUsService } from './../../services/DI/about-us.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss']
 })
-export class AboutUsComponent implements OnInit {
-  list: IAboutUs[];
-
-  constructor(private aboutUsService: AboutUsService) {
-    this.list = this.aboutUsService.getAboutUs();
-  }
-
-  ngOnInit(): void {
-  }
-
+export class AboutUsComponent {
 }
