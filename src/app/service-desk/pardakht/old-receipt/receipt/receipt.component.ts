98e3633b281aed5ef9c609b1aa31a54c604b2ef2
JCln <ghmantera@gmail.com>
2020-08-26 13:32:14 +0430

    receipt UI Changed Totally

--- src/app/service-desk/pardakht/old-receipt/receipt/receipt.component.ts
+++ src/app/service-desk/pardakht/old-receipt/receipt/receipt.component.ts
@@ -30,23 +30,6 @@ export class ReceiptComponent implements AfterViewInit, OnDestroy {
       }
     });
   }
-  fixedWidth = () => {
-    const main = document.querySelector('.main') as HTMLElement;
-    const _numbers1 = document.querySelector('._first') as HTMLElement;
-    const _numbers2 = document.querySelector('._second') as HTMLElement;
-    const _numbers3 = document.querySelector('._third') as HTMLElement;
-    const _numbers4 = document.querySelector('._forth') as HTMLElement;
-
-    main.style.display = "grid";
-    main.style.gridTemplateColumns = "repeat(4, minmax(20rem , 1fr))";
-    main.style.overflow = "auto";
-    main.style.justifyContent = "inherit";
-
-    _numbers1.style.width = "100%";
-    _numbers2.style.width = "100%";
-    _numbers3.style.width = "100%";
-    _numbers4.style.width = "100%";
-  }
   exportAsPrint(): void {
     window.print();
     window.close();
